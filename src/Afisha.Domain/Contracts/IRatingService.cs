b04c259c9873ed05b4322daf5624afb9222f9c9a
Evgenii Chavkin <a110589@yandex.ru>
2024-12-23 00:19:39 +0400

    T24 feat: rating

---
+++ src/Afisha.Domain/Contracts/IRatingService.cs
@@ -0,0 +1,8 @@
+﻿using Afisha.Domain.Entities;
+
+namespace Afisha.Domain.Contracts;
+
+public interface IRatingService
+{
+    Task<Rating> GetLocationByIdAsync(long id, CancellationToken cancellationToken);
+}
---
+++ src/Afisha.Domain/Entities/Rating.cs
@@ -0,0 +1,26 @@
+﻿namespace Afisha.Domain.Entities;
+
+/// <summary>
+/// Отзыв о мероприятии
+/// </summary>
+public class Rating : EntityBase<long>
+{
+    /// <summary>
+    /// Оценка 
+    /// </summary>
+    public int Value { get; set; }
+    //  modelBuilder.Entity<Rating>().ToTable(t => t.HasCheckConstraint("Value", "Age > 1 AND Age < 5"));
+
+    /// <summary>
+    /// Дата создания
+    /// </summary>
+    public DateTime CreateDate { get; set; }  = DateTime.Now;
+    /// <summary>
+    /// Текст отзыва
+    /// </summary>
+    public string? Description { get; set; }
+    /// <summary>
+    /// Пользователь, оставивший отзыв
+    /// </summary>
+    public User User { get; set; }
+}
