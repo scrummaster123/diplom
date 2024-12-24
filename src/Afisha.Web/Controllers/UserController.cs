b2c336f14525629feac0d5dafd84a7c494d012d6
MasalskaiaM <MasalskaiaM@AP29>
2024-12-24 17:15:10 +0300

    Добавила сущность User и сделала методы добавления, получения и удаления на всех слоях. Список новых классов и интерфейсов:
    -User (Domain)
    -IUserService(Domain)
    -UserRepository(Infrastructure)
    -UserService(Application)
    -IUserRepository(Application)
    -UserController(Web)

---
+++ src/Afisha.Web/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Afisha.Domain.Contracts;
+using Afisha.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Afisha.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController(IUserService userService) :  Controller
+    {
+        [HttpPost]
+        [Route("add-user")]
+        public async Task<IActionResult> AddUserAsync([FromBody] User newUser)
+        {
+            var user = await userService.AddUserAsync(newUser, HttpContext.RequestAborted);
+            return Ok(user);
+        }
+
+        [HttpGet]
+        [Route("get-user-by-id")]
+        public async Task<User> GetUserByIdAsync([FromQuery] long id)
+        {
+            return await userService.GetUserByIdAsync(id, HttpContext.RequestAborted);
+        }
+
+        [HttpDelete]
+        [Route("delete-user")]
+        public async Task<IActionResult> DeleteUserAsync(long id)
+        {
+            if( await userService.DeleteUserAsync(id, HttpContext.RequestAborted) == true)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
