b7e440b92151d7369c3a5a892bcd5a633f7c5e42
proninp <proninpaul@yandex.ru>
2024-12-26 22:46:17 +0300

    feat: T-34 Добавлены реализации дженерик репозиториев в проект Infrastructure

---
+++ Afisha/src/Afisha.Infrastructure/Data/Repositories/ReadRepository.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq.Expressions;
+using Afisha.Domain.Entities.Abstractions;
+using Afisha.Domain.Enums;
+using Afisha.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Afisha.Infrastructure.Data.Repositories;
+public class ReadRepository<T, TKey> : IReadRepository<T, TKey>
+    where T : EntityBase<TKey>
+    where TKey : struct
+{
+    private protected AfishaDbContext _context;
+    private protected DbSet<T> _dbSet;
+
+    public ReadRepository(AfishaDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public Task<T[]> Get(
+        Expression<Func<T, bool>>? predicate = null,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+        TrackingType trackingType = TrackingType.NoTracking,
+        CancellationToken cancellationToken = default)
+    {
+        var query = GetQuery(trackingType);
+
+        if (include is not null)
+            query = include(query);
+
+        if (predicate is not null)
+            query = query.Where(predicate);
+
+        if (orderBy is not null)
+            query = orderBy(query);
+
+        return query.ToArrayAsync(cancellationToken);
+    }
+
+    public Task<T?> GetById(
+        TKey id,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+        TrackingType trackingType = TrackingType.NoTracking,
+        CancellationToken cancellationToken = default)
+    {
+        var query = GetQuery(trackingType);
+
+        if (include != null)
+            query = include(query);
+
+        return query.FirstOrDefaultAsync(e => e.Id.Equals(id), cancellationToken);
+    }
+
+    public async Task<T> GetByIdOrThrow(
+    TKey id,
+    Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+    TrackingType trackingType = TrackingType.NoTracking,
+    CancellationToken cancellationToken = default)
+    {
+        var entity = await GetById(id, include, trackingType, cancellationToken);
+        if (entity is null)
+            throw new ArgumentException($"Запись {typeof(T).Name} с id:'{id}' не была найдена.");
+        return entity;
+    }
+
+    private IQueryable<T> GetQuery(TrackingType trackingType) =>
+        trackingType switch
+        {
+            TrackingType.NoTracking => _dbSet.AsNoTracking(),
+            TrackingType.NoTrackingWithIdentityResolution => _dbSet.AsNoTrackingWithIdentityResolution(),
+            TrackingType.Tracking => _dbSet,
+            _ => throw new ArgumentOutOfRangeException(nameof(trackingType), trackingType, null)
+        };
+}
---
+++ Afisha/src/Afisha.Infrastructure/Data/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Afisha.Domain.Entities.Abstractions;
+using Afisha.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Afisha.Infrastructure.Data.Repositories;
+public class Repository<T, TKey> :
+    ReadRepository<T, TKey>,
+    IRepository<T, TKey>
+    where T : EntityBase<TKey>
+    where TKey : struct
+{
+    public Repository(AfishaDbContext context) : base(context) { }
+
+    public T Add(T item)
+    {
+        var addResult = _context.Add(item);
+        return addResult.Entity;
+    }
+
+    public T Update(T item)
+    {
+        var updateResult = _dbSet.Update(item);
+        return updateResult.Entity;
+    }
+
+    public void Delete(T item)
+    {
+        if (_context.Entry(item).State == EntityState.Detached)
+        {
+            _dbSet.Attach(item);
+        }
+        _context.Remove(item);
+    }
+
+    public async Task Delete(TKey id, CancellationToken cancellationToken)
+    {
+        var item = await _dbSet.FirstOrDefaultAsync(u => u.Id.Equals(id));
+        if (item is not null)
+            Delete(item);
+    }
+
+    
+}
