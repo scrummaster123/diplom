b5981b46fac046051db37a405b6251f047729b8a
Evgenii Chavkin <a110589@yandex.ru>
2025-02-10 22:17:14 +0400

    refactor T41

--- Afisha/src/Afisha.Application/Services/Managers/RatingService.cs
+++ Afisha/src/Afisha.Application/Services/Managers/RatingService.cs
@@ -2,31 +2,49 @@
 using Afisha.Application.Specifications;
 using Afisha.Domain.Entities;
 using Afisha.Domain.Entities.Dto;
+using Afisha.Domain.Interfaces;
 using Afisha.Domain.Interfaces.Repositories;
 
 namespace Afisha.Application.Services.Managers;
 
 public class RatingService(
-    IRepository<Rating, long> ratingRepository
+    IRepository<Rating, long> ratingRepository,
+    IRepository<User, long> userRepository,
+    IRepository<Event, long> eventRepository,
+    IUnitOfWork unitOfWork
     ) : IRatingService
 {
     public async Task<Rating> AddRating(RatingDto ratingDto, CancellationToken cancellationToken)
     {
+        var user = await userRepository.GetByIdAsync(ratingDto.UserId);
+        var myEvent = await eventRepository.GetByIdAsync(ratingDto.EventId);
+
+        if (user is null)
+            throw new ArgumentNullException(nameof(user));
+
+        if (myEvent is null)
+            throw new ArgumentNullException(nameof(myEvent));
+
         var rating = ratingRepository.Add(new Rating()
         {
             Value = ratingDto.Value,
             Description = ratingDto.Description,
-            Event = null,
-            User = null
+            Event = myEvent,
+            User = user
         });
+
+        await unitOfWork.CommitAsync(cancellationToken);
 
         return rating;
     }
 
-    public Task<List<RatingDto>> GetRatingsByEvent(long eventId, CancellationToken cancellationToken)
+    public async Task<List<RatingDto>> GetRatingsByEvent(long eventId, CancellationToken cancellationToken)
     {
-        var result = ratingRepository.GetAsync(new SpecificationBase<Rating>(rating => rating.Event.Id == eventId), cancellationToken: cancellationToken);
+        var ratings = await ratingRepository.GetAsync(new SpecificationBase<Rating>(rating => rating.Event.Id == eventId), cancellationToken: cancellationToken);
 
-        throw new NotImplementedException();
+        var result = ratings.Select(r => new RatingDto() { Description = r.Description, Value = r.Value, EventId = r.Event.Id, UserId = r.User.Id })
+                            .ToList();
+
+        return result;
     }
 }
