f31b39fe96c0b9b74b5da2ca5ead46dcc96f4f72
Sergey Seliverstov <clonx@mail.ru>
2025-02-11 09:22:57 +0400

    feat: T-39 EventController with DTO classes

---
+++ Afisha/src/Afisha.Application/Specifications/Event/EventWithUserAndLocation.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Afisha.Application.Specifications.Event
+{
+    public class EventWithUserAndLocation : SpecificationIncludeBase<Domain.Entities.Event>
+    {
+        public EventWithUserAndLocation() : base(
+            location => location.Include(e => e.Sponsor)
+                .Include(e => e.Location)
+            )
+        {}
+    }
+}
--- Afisha/src/Afisha.Web/Infrastructure/Configuration/ConfigureCoreServices.cs
+++ Afisha/src/Afisha.Web/Infrastructure/Configuration/ConfigureCoreServices.cs
@@ -1,4 +1,5 @@
-﻿using Afisha.Application.Services.Interfaces;
+﻿using Afisha.Application.Mappers;
+using Afisha.Application.Services.Interfaces;
 using Afisha.Application.Services.Managers;
 using Afisha.Domain.Interfaces;
 using Afisha.Domain.Interfaces.Repositories;
@@ -16,6 +17,8 @@ public static class ConfigureCoreServices
         services.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
         services.AddScoped<ILocationService, LocationService>();
         services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IEventService, EventService>();
+        services.AddSingleton<AutoMapperConfiguration>();
         return services;
     }
 
