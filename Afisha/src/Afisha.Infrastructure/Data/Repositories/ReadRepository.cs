858ee788f6535128288f1a543be62053a98147be
Sergey Seliverstov <clonx@mail.ru>
2025-02-12 19:58:14 +0400

    refactor: T46 Рефакторинг репозиториев. Включить трекинг по умолчанию

--- Afisha/src/Afisha.Infrastructure/Data/Repositories/ReadRepository.cs
+++ Afisha/src/Afisha.Infrastructure/Data/Repositories/ReadRepository.cs
@@ -20,20 +20,20 @@ public class ReadRepository<T, TKey> : IReadRepository<T, TKey>
     }
 
     /// <summary>
-    /// Получает <see cref="{T}[]"/> основываясь на спецификации. По-умолчанию работает, как no-tracking запрос.
+    /// Получает <see cref="{T}[]"/> основываясь на спецификации. По-умолчанию работает, как tracking запрос.
     /// </summary>
     /// <param name="specification">Спецификация</param>
     /// <param name="trackingType"><c>NoTracking</c> для отключения кеширования; <c>Tracking</c> для включения кеширования; <c>NoTrackingWithIdentityResolution</c> для отключения кеширования, но с вычислением одинаковых сущностей в результате запроса</param>
     /// <param name="cancellationToken">Токен отмены</param>
     /// <returns><see cref="{T}[]"/>, который содержит элементы, соответствующие условию, переданному в <paramref name="specification"/></returns>
-    public async Task<T[]> GetAsync(ISpecification<T> specification, TrackingType trackingType = TrackingType.NoTracking, CancellationToken cancellationToken = default)
+    public async Task<T[]> GetAsync(ISpecification<T> specification, TrackingType trackingType = TrackingType.Tracking, CancellationToken cancellationToken = default)
     {
         var query = specification.BuildQueryable(GetTrackingConfiguredQuery(trackingType));
         return await query.ToArrayAsync(cancellationToken);
     }
 
     /// <summary>
-    /// Получает <see cref="{T}[]"/> основываясь на спецификации и пагинации. По-умолчанию работает, как no-tracking запрос.
+    /// Получает <see cref="{T}[]"/> основываясь на спецификации и пагинации. По-умолчанию работает, как tracking запрос.
     /// </summary>
     /// <param name="specification">Спецификация</param>
     /// <param name="trackingType"><c>NoTracking</c> для отключения кеширования; <c>Tracking</c> для включения кеширования; <c>NoTrackingWithIdentityResolution</c> для отключения кеширования, но с вычислением одинаковых сущностей в результате запроса</param>
@@ -42,7 +42,7 @@ public class ReadRepository<T, TKey> : IReadRepository<T, TKey>
     /// <param name="cancellationToken">Токен отмены</param>
     /// <returns><see cref="{T}[]"/>, который содержит элементы, соответствующие условию, переданному в <paramref name="specification"/></returns>
     /// <exception cref="ArgumentException">Ошибка указания параметров пагинации запроса</exception>
-    public async Task<T[]> GetPagedAsync(ISpecification<T> specification, TrackingType trackingType = TrackingType.NoTracking, int pageIndex = 0, int pageSize = 20, CancellationToken cancellationToken = default)
+    public async Task<T[]> GetPagedAsync(ISpecification<T> specification, TrackingType trackingType = TrackingType.Tracking, int pageIndex = 0, int pageSize = 20, CancellationToken cancellationToken = default)
     {
         if (pageIndex < 0)
             throw new ArgumentException("Индекс страницы должен быть больше, либо равен 0");
@@ -59,14 +59,14 @@ public class ReadRepository<T, TKey> : IReadRepository<T, TKey>
     }
 
     /// <summary>
-    /// Получает <see cref="{T?}"/> основываясь Id сущности и include делегате. По-умолчанию работает, как no-tracking запрос.
+    /// Получает <see cref="{T?}"/> основываясь Id сущности и include делегате. По-умолчанию работает, как tracking запрос.
     /// </summary>
     /// <param name="id">Id искомой сущности</param>
     /// <param name="specification">Спецификация</param>
     /// <param name="trackingType"><c>NoTracking</c> для отключения кеширования; <c>Tracking</c> для включения кеширования; <c>NoTrackingWithIdentityResolution</c> для отключения кеширования, но с вычислением одинаковых сущностей в результате запроса</param>
     /// <param name="cancellationToken">Токен отмены</param>
     /// <returns><see cref="{T?}"/> с Id, переданным в параметре <paramref name="id"/></returns>
-    public async Task<T?> GetByIdAsync(TKey id, IIncludeSpecification<T>? specification = null, TrackingType trackingType = TrackingType.NoTracking, CancellationToken cancellationToken = default)
+    public async Task<T?> GetByIdAsync(TKey id, IIncludeSpecification<T>? specification = null, TrackingType trackingType = TrackingType.Tracking, CancellationToken cancellationToken = default)
     {
         var query = GetTrackingConfiguredQuery(trackingType);
         if (specification != null)
@@ -75,7 +75,7 @@ public class ReadRepository<T, TKey> : IReadRepository<T, TKey>
     }
 
     /// <summary>
-    /// Получает <see cref="{T}"/> основываясь Id сущности и спецификации. По-умолчанию работает, как no-tracking запрос.
+    /// Получает <see cref="{T}"/> основываясь Id сущности и спецификации. По-умолчанию работает, как tracking запрос.
     /// </summary>
     /// <param name="id">Id искомой сущности</param>
     /// <param name="specification">Спецификация</param>
@@ -83,7 +83,7 @@ public class ReadRepository<T, TKey> : IReadRepository<T, TKey>
     /// <param name="cancellationToken">Токен отмены</param>
     /// <returns><see cref="{T}"/> с Id, переданным в параметре <paramref name="id"/></returns>
     /// <exception cref="ArgumentException">Ошибка, если элемент <see cref="{T}"/> не найден</exception>
-    public async Task<T> GetByIdOrThrowAsync(TKey id, IIncludeSpecification<T>? specification = null, TrackingType trackingType = TrackingType.NoTracking, CancellationToken cancellationToken = default)
+    public async Task<T> GetByIdOrThrowAsync(TKey id, IIncludeSpecification<T>? specification = null, TrackingType trackingType = TrackingType.Tracking, CancellationToken cancellationToken = default)
     {
         var entity = await GetByIdAsync(id, specification, trackingType, cancellationToken);
         if (entity is null)
